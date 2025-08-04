9bcf3dd6f81414dcc1a2222457ae7d92b5308694
Matt Wittmann <mcwitt@gmail.com>
2025-08-04 12:06:22 -0700

    Add pre-commit

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   inputs.ghc-wasm-meta.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
+  inputs.git-hooks.url = "github:cachix/git-hooks.nix";
 
   outputs =
-    {
-      self,
-      ghc-wasm-meta,
+    { self
+    , ghc-wasm-meta
+    , git-hooks
+    ,
     }:
     let
       systems = [
@@ -19,27 +21,43 @@
       forAllSystems = f: forAllSystems' (system: f (mkPkgs system) system);
     in
     {
+      checks = forAllSystems' (system: {
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            cabal-fmt.enable = true;
+            hlint.enable = true;
+            ormolu.enable = true;
+
+            html-tidy.enable = true;
+
+            nixfmt-rfc-style.enable = true;
+          };
+        };
+      });
+
       devShells = forAllSystems (
         pkgs: system:
-        let
-          haskellPackages = pkgs.haskell.packages.ghc912;
-        in
-        {
-          default = pkgs.mkShellNoCC {
-            packages = [
-              ghc-wasm-meta.packages."${system}".all_9_12
+          let
+            haskellPackages = pkgs.haskell.packages.ghc912;
+          in
+          {
+            default = pkgs.mkShellNoCC {
+              inherit (self.checks.${system}.pre-commit-check) shellHook;
+              packages = [
+                ghc-wasm-meta.packages."${system}".all_9_12
 
-              pkgs.wabt
+                pkgs.wabt
 
-              haskellPackages.ghc
-              haskellPackages.cabal-install
-              haskellPackages.ormolu
-              haskellPackages.haskell-language-server
+                haskellPackages.ghc
+                haskellPackages.cabal-install
+                haskellPackages.haskell-language-server
 
-              pkgs.python3 # for dev http server
-            ];
-          };
-        }
+                pkgs.python3 # for dev http server
+              ]
+              ++ self.checks.${system}.pre-commit-check.enabledPackages;
+            };
+          }
       );
     };
 }
