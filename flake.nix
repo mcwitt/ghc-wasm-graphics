10c4239a4c5ef2f5d8c2dccb234d84f395e024b1
Matt Wittmann <mcwitt@gmail.com>
2025-08-05 10:08:26 -0700

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.ghc-wasm-meta.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
+  inputs.ghc-wasm-meta.inputs.nixpkgs.follows = "nixpkgs";
   inputs.git-hooks.url = "github:cachix/git-hooks.nix";
 
   outputs =
-    { self
-    , ghc-wasm-meta
-    , git-hooks
-    ,
+    {
+      self,
+      nixpkgs,
+      ghc-wasm-meta,
+      git-hooks,
     }:
     let
       systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
-
-      inherit (ghc-wasm-meta.inputs) nixpkgs;
 
       forAllSystems' = nixpkgs.lib.genAttrs systems;
       mkPkgs = system: nixpkgs.legacyPackages.${system};
@@ -38,26 +39,25 @@
 
       devShells = forAllSystems (
         pkgs: system:
-          let
-            haskellPackages = pkgs.haskell.packages.ghc912;
-          in
-          {
-            default = pkgs.mkShellNoCC {
-              inherit (self.checks.${system}.pre-commit-check) shellHook;
-              packages = [
-                ghc-wasm-meta.packages."${system}".all_9_12
+        let
+          haskellPackages = pkgs.haskell.packages.ghc912;
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            packages = [
+              ghc-wasm-meta.packages."${system}".all_9_12
 
-                pkgs.wabt
+              pkgs.wabt
 
-                haskellPackages.ghc
-                haskellPackages.cabal-install
-                haskellPackages.haskell-language-server
+              haskellPackages.ghc
+              haskellPackages.cabal-install
+              haskellPackages.haskell-language-server
 
-                pkgs.python3 # for dev http server
-              ]
-              ++ self.checks.${system}.pre-commit-check.enabledPackages;
-            };
-          }
+              pkgs.python3 # for dev http server
+            ] ++ self.checks.${system}.pre-commit-check.enabledPackages;
+          };
+        }
       );
     };
 }
